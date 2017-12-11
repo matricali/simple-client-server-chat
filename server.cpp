45a53571bb84f975d892f9ed457f412eb716c752
Jorge Matricali <jorgematricali@gmail.com>
2017-12-11 01:07:53 -0300

    + Accept multiple connections

--- server.cpp
+++ server.cpp
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
+#include <vector>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+
+/* Declarations */
+struct client_type
+{
+    int id;
+    int sockfd;
+};
+
+const int INVALID_SOCKET = -1;
+const int MAX_CLIENTS = 5;
+
+
+/* Function prototypes */
+// ...
+
 
 int main(int argc, char** argv)
 {
     int sockfd;
     int port_no;
+    int num_clients = 0;
+    int temp_id = -1;
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     socklen_t client_len;
     char buffer[256];
+    std::vector<client_type> client(MAX_CLIENTS);
+    std::string msg = "";
 
+    /* Banner */
     fprintf(stdout, "simple-chat-server v0.0.1-alpha\n");
 
     if (argc < 2) {
@@ -56,39 +77,84 @@ int main(int argc, char** argv)
     listen(sockfd, 5);
     printf("Listening on %s:%d\n", &server_addr.sin_addr.s_addr, port_no);
 
+    /* Initialize the client list */
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        client[i] = (client_type) { -1, INVALID_SOCKET };
+    }
+
     client_len = sizeof(client_addr);
 
-    /* Accept new connection */
-    int incoming;
-    incoming = accept(sockfd, (struct sockaddr *) &client_addr, &client_len);
-    if (incoming < 0) {
-        fprintf(stderr, "ERROR on accept\n");
-        return -5;
+    while (true) {
+        /* Accept new connection */
+        int incoming = INVALID_SOCKET;
+        incoming = accept(sockfd, (struct sockaddr *) &client_addr, &client_len);
+
+        if (incoming == INVALID_SOCKET) {
+            continue;
+        }
+
+        /* Reset the number of clients */
+        num_clients = -1;
+
+        /* Create a temporary id for the next client */
+        temp_id = -1;
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (client[i].sockfd == INVALID_SOCKET && temp_id == -1) {
+                client[i].sockfd = incoming;
+                client[i].id = i;
+                temp_id = i;
+            }
+
+            if (client[i].sockfd != INVALID_SOCKET) {
+                num_clients++;
+            }
+        }
+
+        if (temp_id != -1) {
+
+            char remote_host[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &(client_addr.sin_addr), remote_host, INET_ADDRSTRLEN);
+            printf(
+                "Connection accepted from %s:%d (Client #%d) accepted\n",
+                remote_host,
+                ntohs(client_addr.sin_port),
+                client[temp_id].id
+            );
+
+            /* Send the id to that client */
+            msg = std::to_string(client[temp_id].id);
+            send(client[temp_id].sockfd, msg.c_str(), strlen(msg.c_str()), 0);
+
+            /* Create a thread process for that client */
+            // my_thread[temp_id] = std::thread(process_client, std::ref(client[temp_id]), std::ref(client), std::ref(my_thread[temp_id]));
+        } else {
+            msg = "Server is full";
+            send(incoming, msg.c_str(), strlen(msg.c_str()), 0);
+            printf("%s\n", msg.c_str());
+        }
+        //
+        // /* Sending hello :D */
+        // send(incoming, "Hello :D\n", 9, 0);
+        //
+        // bzero(buffer, 256);
+        //
+        // int n;
+        // n = read(incoming, buffer, 255);
+        // if (n < 0) {
+        //     fprintf(stderr, "ERROR reading from socket\n");
+        //     return -5;
+        // }
+        //
+        // printf("Message received: %s\n", buffer);
     }
-
-    /* Got connection */
-    char remote_host[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(client_addr.sin_addr), remote_host, INET_ADDRSTRLEN);
-    printf("Got connection from %s port %d\n", remote_host, ntohs(client_addr.sin_port));
-
-
-    /* Sending hello :D */
-    send(incoming, "Hello :D\n", 9, 0);
-
-    bzero(buffer, 256);
-
-    int n;
-    n = read(incoming, buffer, 255);
-    if (n < 0) {
-        fprintf(stderr, "ERROR reading from socket\n");
-        return -5;
-    }
-
-    printf("Message received: %s\n", buffer);
 
     /* Closing */
-    close(incoming);
     close(sockfd);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        // my_thread[i].detach();
+        close(client[i].sockfd);
+    }
 
     return 0;
 }
